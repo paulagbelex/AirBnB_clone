dd3f23508cacaf25b747fe0fed414a7c396aee52
Cassyeh <cassandraijezie@gmail.com>
2023-05-14 21:14:51 -0700

    Pycode style for first test on basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,14 +7,14 @@ from models.base_model import BaseModel
 from datetime import datetime
 
 
-
 class TestBaseModel(unittest.TestCase):
-  """
-  Test Cases for the BaseModel class.
-  """
-  def test_1(self):
+    """
+    Test Cases for the BaseModel class.
+    """
+    def test_1(self):
         my_model = BaseModel()
-        self.assertEqual(str(type(my_model)), "<class 'models.base_model.BaseModel'>")
+        self.assertEqual(str(type(
+            my_model)), "<class 'models.base_model.BaseModel'>")
         self.assertIsInstance(my_model, BaseModel)
         self.assertTrue(issubclass(type(my_model), BaseModel))
         my_model.name = "My First Model"
@@ -35,8 +35,10 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn("__class__", my_model_json)
         self.assertEqual(type(my_model_json["updated_at"]), str)
         self.assertEqual(type(my_model_json["created_at"]), str)
-        self.assertEqual(my_model_json["created_at"], my_model.created_at.isoformat())
-        self.assertEqual(my_model_json["updated_at"], my_model.updated_at.isoformat())
+        self.assertEqual(my_model_json["created_at"], (
+            my_model.created_at.isoformat()))
+        self.assertEqual(my_model_json["updated_at"], (
+            my_model.updated_at.isoformat()))
         self.assertEqual(my_model_json["name"], my_model.name)
         self.assertEqual(my_model_json["my_number"], my_model.my_number)
         self.assertEqual(type(my_model.created_at), datetime)
@@ -44,7 +46,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertRaises(TypeError, BaseModel, 1)
         self.assertRaises(TypeError, BaseModel.__init__)
         self.assertNotIsInstance(my_model_json, BaseModel)
-    
-        
+
+
 if __name__ == '__main__':
     unittest.main()
