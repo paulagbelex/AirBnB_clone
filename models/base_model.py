b2f4bb52c4ceeb323cd1a97364678adf9803d676
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-05-15 05:26:50 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -16,9 +16,22 @@ class BaseModel():
         """
         Function that initializes BaseModel
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None or kwargs != {}:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at":
+                    self.created_at = (datetime.strptime(
+                    self.created_at, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key == "updated_at":
+                    self.updated_at = (datetime.strptime(
+                    self.updated_at, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
