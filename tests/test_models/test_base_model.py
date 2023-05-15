e56ff8800c876c2aca3bc27fe125d02dc801457c
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-05-15 05:46:13 +0100

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,6 +17,9 @@ class TestBaseModel(unittest.TestCase):
             my_model)), "<class 'models.base_model.BaseModel'>")
         self.assertIsInstance(my_model, BaseModel)
         self.assertTrue(issubclass(type(my_model), BaseModel))
+        self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "save"))
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
         my_model.name = "My First Model"
         my_model.my_number = 89
         self.assertEqual(my_model.my_number, 89)
@@ -46,6 +49,21 @@ class TestBaseModel(unittest.TestCase):
         self.assertRaises(TypeError, BaseModel, 1)
         self.assertRaises(TypeError, BaseModel.__init__)
         self.assertNotIsInstance(my_model_json, BaseModel)
+        # Test when kwargs is not empty
+        kwargs = {"id": "1234", "created_at": "2022-05-01T10:00:00.000000",
+                "updated_at": "2022-05-02T10:00:00.000000"}
+        b2 = BaseModel(**kwargs)
+        self.assertEqual(b2.id, "1234")
+        self.assertEqual(str(b2.created_at), "2022-05-01 10:00:00")
+        self.assertEqual(str(b2.updated_at), "2022-05-02 10:00:00")
+        my_model = BaseModel()
+        my_model.name = "My_First_Model"
+        my_model.my_number = 89
+        my_model_json = my_model.to_dict()
+        my_new_model = BaseModel(**my_model_json)
+        self.assertEqual(my_new_model.id, my_model.id)
+        self.assertEqual(type(my_new_model.created_at), datetime)
+        self.assertFalse(my_model is my_new_model)
 
 
 if __name__ == '__main__':
