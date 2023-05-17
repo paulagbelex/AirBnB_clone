2c5dd7723b2ef81c459c20531b1fed5f78738992
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-05-17 06:03:04 +0100

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Contains a class State
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Class User that inherits from BaseModel
+    """
+    name = ""
