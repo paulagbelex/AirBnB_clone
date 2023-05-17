208e21d8256bec534e662e71278844b68aea5c76
Cassyeh <117908223+Cassyeh@users.noreply.github.com>
2023-05-17 06:04:01 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -59,6 +59,11 @@ class FileStorage():
         """
         from models.base_model import BaseModel
         from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
         if os.path.isfile(FileStorage.__file_path) is not True:
             return
         temp = {}
